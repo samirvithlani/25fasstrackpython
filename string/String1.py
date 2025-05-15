8ca47d831c4479a6478e077d43271b423a178142
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-15 11:28:39 +0530

    string

---
+++ string/String1.py
@@ -0,0 +1,33 @@
+name = "Royal Technosoft"
+print(name)
+
+#immutable
+#name[0] = "r" #TypeError: 'str' object does not support item assignment
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+#string lenth find..
+
+
+length = len(name)
+print("length ",length)
+
+#for i in range(0,length):
+#for i in range(0,len(name)):
+# for i in range(len(name)):
+#     print(name[i])
+
+#print(name)
+
+# for i in name:
+#     print(i)
+    
+
+x = 65   
+print('chr...',chr(x))
+
+y ='a'
+print("y..",ord(y))
