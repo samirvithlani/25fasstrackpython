e668af9fa6d662e6d7270150c6e9a551d61046fb
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-16 12:40:40 +0530

    string done

--- string/String11.py
+++ string/String11.py
@@ -3,7 +3,8 @@ data = "ahmedabad"
 #x = data[1:3]
 #x = data[1:6:2] #hea 1 skip
 #x = data[::-1]# ahmedabad dabademha
-x = data[4::-1] # ahmedabad
+#x = data[4::-1] # ahmedabad
+x = data[1:1]
 print(x)
 
 name = input("enter name")
