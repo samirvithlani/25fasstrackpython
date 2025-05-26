929c2705b76772a725ae6c38c6a841f0033136ca
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-26 11:57:49 +0530

    set

---
+++ ds/set/setDemo2.py
@@ -0,0 +1,23 @@
+user1 = {"ram","shyam","amit","sumit","ajay"}
+user2 = {"ram","shyam","amit","kunal","parth"}
+
+x = user1.union(user2) #
+print(x)
+
+x = user1.difference(user2) #
+print(x)
+
+x = user1.intersection(user2)
+print(x)
+
+x= user1.issuperset(user2) #
+print(x)
+
+x = user1.issubset(user2) #
+print(x)
+
+x = user1.symmetric_difference(user2)
+print(x)
+
+x = user1.isdisjoint(user2)
+print(x)
---
+++ ds/set/setDemo3.py
@@ -0,0 +1,5 @@
+user1 = {"ram","shyam","amit","sumit","ajay"}
+user2 = {"ram","shyam","amit","kunal","parth"}
+
+user1.difference_update(user2)
+print(user1)
