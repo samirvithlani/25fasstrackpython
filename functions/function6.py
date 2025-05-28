dc4571d32ef98f1f9185bc18d688562844611eae
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-28 11:54:47 +0530

    kwargs

---
+++ functions/function6.py
@@ -0,0 +1,9 @@
+# def getStudentDetails(**kwargs):
+#     print(kwargs)
+
+def getStudentDetails(x,**kwargs):
+    print(kwargs)
+    print(x)
+
+
+getStudentDetails(name="ram",age=23,status=True,city="ahmedbad",x=100)    
---
+++ functions/function7.py
@@ -0,0 +1,7 @@
+def demo(*args,**kwargs):
+    print(args)
+    print(kwargs)
+
+
+demo(10,True,"ok",name="java",price=1000) 
+
