17071f53527794a97fd1cae5890b0bef6d744aef
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-30 12:37:45 +0530

    lambda

---
+++ functions/lambda/lambda1.py
@@ -0,0 +1,23 @@
+#lambda function --> Annonymous function
+#lambda arguments : expression
+
+# def test(x):
+#     print(x)
+
+# test = lambda x:print(x) #function
+# test(100)
+
+
+# def test(a,b):
+#     print(a+b)
+
+# test = lambda a,b : print(a+b)
+# test(10,20)
+
+# def test(a,b,c):
+#     return a + b + c
+
+# print(test(10,20,30))
+
+test = lambda a,b,c : a+b+c # : a+ b+ c --return
+print(test(10,20,30))
