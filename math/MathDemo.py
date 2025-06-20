4d4ddb52ea83a9305013596aa312bea3ef0eb482
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-20 11:30:26 +0530

    math

---
+++ math/MathDemo.py
@@ -0,0 +1,23 @@
+import math
+
+#sqr root
+x = math.sqrt(144)
+print(x)
+print(math.pow(2,10))
+
+print(math.factorial(5)) #5 4 3 2 1
+print(math.ceil(5.1))
+print(math.floor(81.23))
+print(math.fabs(-7.2)) #abs value..
+print(math.log(10))
+print(math.log10(100))
+print(math.exp(1))
+print(math.gcd(12,18)) #6
+
+#trigonomerric functions
+#sin,cos,tan,radians,degrees
+
+#contant
+#pi,e,inf,nan
+
+#get redius from user and find circle area using math lib
