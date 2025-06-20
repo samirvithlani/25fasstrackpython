9397ccc141c86b78c38f7bd51bf4f506f3a8defb
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-20 11:45:26 +0530

    math

---
+++ math/MathDemo2.py
@@ -0,0 +1,10 @@
+import math
+print(math.modf(5.75))
+
+print(math.copysign(1.0,100))
+print(math.isclose(100+20,120))
+
+print(math.remainder(10,2))
+print(math.comb(6,3))
+print(math.perm(5,2))
+print(math.lcm(12,18))
